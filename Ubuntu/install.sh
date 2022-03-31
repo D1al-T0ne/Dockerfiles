a78fdffc78d8d778fe0c1ae910b7a0374c473600
Jess <d1al.t0ne@outlook.com>
2022-03-31 15:33:57 -0400

    Create install.sh

---
+++ Ubuntu/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# install python libraries
+pip3 install ipython
+
+# customization
+cd /root/
+wget https://raw.githubusercontent.com/d1al-t0ne/dockerfiles/main/dotfiles/.vimrc
+wget https://raw.githubusercontent.com/d1al-t0ne/dockerfiles/main/dotfiles/update.sh
